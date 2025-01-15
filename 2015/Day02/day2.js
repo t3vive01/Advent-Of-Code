8ceb6cdf95d4dfa22d79657558cf8b5b72bdd108
t3vive01 <t3vive01@students.oamk.fi>
2025-01-15 22:01:39 +0200

    Day 2 done

--- 2015/Day02/day2.js
+++ 2015/Day02/day2.js
@@ -1,3 +1,29 @@
 const fs = require('node:fs');
 
-const input = fs.readFileSync('Day01/input.txt').toString()
+const input = fs.readFileSync('Day02/input.txt').toString();
+
+
+const dimensions = input.trim().split('\n');
+
+let paper = 0; 
+let ribbon = 0; 
+
+
+dimensions.forEach(dim => {
+    let [l, w, h] = dim.split('x').map(Number);
+
+    const [side1, side2, side3] = [l * w, w * h, h * l];
+
+    let surfaceArea = 2 * (side1 + side2 + side3);
+    let smallestSide = Math.min(side1, side2, side3);
+    
+    paper += surfaceArea + smallestSide;
+
+   
+    let smallestPerimeter = 2 * (l + w + h - Math.max(l, w, h)); 
+    let volume = l * w * h;
+    ribbon += smallestPerimeter + volume;
+});
+
+console.log("Wrapping paper needed:", paper);
+console.log("Ribbon needed:", ribbon);
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Advent of code
+
+
+```sh
+cd 2015
+node Day01/day1.js
+```
