7e853a74b9ba7308a2ec8962ec4383cfff3fb2d3
t3vive01 <t3vive01@students.oamk.fi>
2025-01-19 22:56:04 +0200

    Day 3 of 2015 done

--- 2015/Day03/day03.js
+++ 2015/Day03/day03.js
@@ -20,5 +20,54 @@ function uniqueHousesVisited(directions) {
     return visited.size;
 }
 
+function uniqueHousesVisited2(directions) {
+    let santaX = 0, santaY = 0;
+    let roboX = 0, roboY = 0;
+    const visited = new Set();
+    visited.add(`${santaX},${santaY}`);
+
+    for (let i = 0; i < directions.length; i++) {
+        const direction = directions[i];
+        if (i % 2 === 0) {
+            switch (direction) {
+                case '^':
+                    santaY++;
+                    break;
+                case 'v':
+                    santaY--;
+                    break;
+                case '>':
+                    santaX++;
+                    break;
+                case '<':
+                    santaX--;
+                    break;
+            }
+            visited.add(`${santaX},${santaY}`);
+        } else {
+            switch (direction) {
+                case '^':
+                    roboY++;
+                    break;
+                case 'v':
+                    roboY--;
+                    break;
+                case '>':
+                    roboX++;
+                    break;
+                case '<':
+                    roboX--;
+                    break;
+            }
+            visited.add(`${roboX},${roboY}`);
+        }
+    }
+
+    return visited.size;
+}
 const result = uniqueHousesVisited(input.trim());
 console.log(`Number of houses visited: ${result}`);
+
+
+const result2 = uniqueHousesVisited2(input.trim());
+console.log(`Houses that got at least one present: ${result2}`);
